ae735ea0dadcbfa6257482111f4dfd6150bb50be
COlin Heggli <colin.heggli@gmail.com>
2023-10-23 21:42:35 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -7,15 +7,9 @@ branch="main"
 
 if git fetch origin "$branch" && [ "$(git rev-list HEAD...origin/"$branch" --count)" -eq 0 ]; then
     echo "The Git repository is up to date."
-    echo "Docker image will not be rebuilt."
     # shellcheck disable=SC2162
-    if not [ "$1" = "-rebuild" ] || not [ "$2" = "-rebuild" ]; then
-        read -p "Rebuild? (Y/N): " confirm
-        if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
-            rebuild=true
-        else
-            rebuild=false
-        fi
+    if not ["-rebuild" in sys.argv] || not "-rebuild" in sys.argv; then
+        rebuild=false
     else
         rebuild=true
     fi
@@ -23,23 +17,27 @@ else
     rebuild=true
 fi
 
-if [ "$rebuild" = true ] || [ "$1" = "-rebuild" ] || [ "$2" = "-rebuild" ]; then
+if [ "$rebuild" = true ] || "-rebuild" in sys.argv; then
     echo "The Git repository is not up to date."
     echo "Pulling the latest changes..."
     git pull origin "$branch"
-    sleep 5
-    echo "Deleting the old Docker image..."
-    sudo docker image rm -f apihub
-    echo "Building the Docker image..."
-    sudo docker build --no-cache -t apihub .
     echo "Deleting the old Docker container..."
     sudo docker rm -f apihub
+    echo "Deleting the old Docker image..."
+    sudo docker images rm -f apihub
+    echo "Building the Docker image..."
+    sudo docker build --no-cache -t apihub .
     echo "Running the new Docker container..."
     sudo docker run -d -p 6969:6969 -v config:/app/config --restart unless-stopped --name apihub apihub
 fi
-if [ "$1" = "-logs" ] || [ "$2" = "-logs" ]; then
+if "-logs" in sys.argv ; then
     echo "Showing the logs..."
     sudo docker logs -f apihub
+fi
+
+if "-cleanup" in sys.argv ; then
+    echo "Cleaning up..."
+    sudo docker images -a | grep "none" | awk '{print $3}' | xargs sudo docker image rm -f
 fi
 
 echo "Done."
