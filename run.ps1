5c8326072e2ffa2878a37d42a6c985498498efdc
Colin <colin.heggli@bbz-sh.ch>
2023-10-24 09:36:20 +0200

    run.ps1 fix

--- run.ps1
+++ run.ps1
@@ -1,36 +1,41 @@
-function main()
-{
+function main() {
     Clear-Host
-    Write-Host "
- _____  _    _ _   _   ______ _ _
+    Write-Host @"
+  _____  _    _ _   _   ______ _ _
  |  __ \| |  | | \ | | |  ____(_) |
  | |__) | |  | |  \| | | |__   _| | ___
  |  _  /| |  | | . ` | |  __| | | |/ _ \
  | | \ \| |__| | |\  | | |    | | |  __/
- |_|  \_\\____/|_| \_| |_|    |_|_|\___|
+ |_|  \_\____/|_| \_| |_|    |_|_|\___|
+"@
 
-
-
-"
     Write-Host "Which RUN file do you want to run?"
     Write-Host "    All Tools           [all]"
     Write-Host "    Ping Tool           [ping]"
     Write-Host "    CTT Tool            [ctt]"
-    #
 }
-$select = Read-Host ">> ".lower()
 
-Switch ($select)
-{
-    ("All Tools") {
+# Calling the main function
+main
+
+# Read user input and convert it to lowercase
+$select = (Read-Host ">> ").ToLower()
+
+# Using the switch statement to determine the action based on user input
+switch ($select) {
+    ("all") {
         Write-Host 'Starting All Tools...'
-        irm api.heggli.dev/all_tools | iex
-    }   ("ping") {
+        irm 'https://example.com/all_tools.ps1' | iex
+    }
+    ("ping") {
         Write-Host "Starting Ping Tool..."
-        irm api.heggli.dev/ping | iex
+        irm 'https://example.com/ping_tool.ps1' | iex
     }
     ("ctt") {
         Write-Host "Starting CTT Tool..."
-        irm christitus.com/win | iex
+        irm 'https://example.com/ctt_tool.ps1' | iex
+    }
+    default {
+        Write-Host "Invalid selection. Please choose 'all', 'ping', or 'ctt'."
     }
 }
